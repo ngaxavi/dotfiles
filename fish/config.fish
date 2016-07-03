5a1e4bbba5dde08deadb6ad0ea156a3780353270
Xavier Ngansop <ngaxavi@gmail.com>
2016-07-03 19:53:43 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,15 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/home/ngansop/.local/share/omf"
+set -gx OMF_PATH "/home/username/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/ngansop/.config/omf"
+#set -gx OMF_CONFIG "/home/username/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-# Path to kasselClient library
-set -x LD_LIBRARY_PATH /media/data/Workspaces/GitHub/kasselClient/lib
 
 # Base16 Shell
+# Check base16 on github
 eval sh $HOME/.config/base16-shell/base16-default.dark.sh
 
 # Path
