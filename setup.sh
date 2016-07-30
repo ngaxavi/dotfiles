ef1b63f0bb96acd6bad05a218425652c1f3875ee
ngaxavi <xng@cs.uni-kassel.de>
2016-07-30 13:34:30 +0200

    fix path to ansible directory

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ BASEDIR=$(dirname $0)
 . $BASEDIR/inc/functions
 
 explain "Install ansible"
-  tell sudo pacman -Sy ansible --noconfirm --needed
+  tell sudo pacman -Syu ansible --noconfirm --needed
 
 explain "Change to ansible Installation"
-  tell cd ../ansible
+  tell cd ansible
 
 explain "Start Installation"
   tell ansible-playbook -i "local," install.yml
