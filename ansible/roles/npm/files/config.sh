84232aea13590f99f7939cbf4c9ebb6a5bc2fc85
Xavier Ngansop <ngaxavi@gmail.com>
2018-01-10 01:04:14 +0100

    update dotfiles

--- ansible/roles/npm/files/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir /home/{{username}}/.npm-global
-npm config set prefix '/home/{{username}}/.npm-global'
-
-
-echo export PATH=~/.npm-global/bin:$PATH > /home/{{username}}/.profile
-source /home/{{username}}/.profile
