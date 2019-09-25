0c37b994478643ae25c7d515f64a4fc0752e4dd7
Xavier Ngansop <ngaxavi@gmail.com>
2019-09-25 15:45:15 +0200

    add some roles to install many stuff automatically

--- setup.sh
+++ setup.sh
@@ -4,10 +4,13 @@ BASEDIR=$(dirname $0)
 . $BASEDIR/inc/functions
 
 explain "Install ansible"
-  tell sudo pacman -S ansible --noconfirm --needed
+  tell pacman -Syy python-passlib ansible --noconfirm --needed
+
+explain "Install and Update the submodules"
+  tell  git submodule init && git submodule update
 
 explain "Change to ansible Installation"
   tell cd ansible
 
 explain "Start Installation"
-  tell ansible-playbook -i "local," install.yml
+  tell ansible-playbook -i localhost playbook.yml
