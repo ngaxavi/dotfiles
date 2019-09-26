47e77d28dd61a9a4caeeecf8c5696c0c8f05d31a
Xavier Ngansop <ngaxavi@gmail.com>
2019-09-27 01:22:53 +0200

    fix base-setup

--- base-setup-bios.sh
+++ base-setup-bios.sh
@@ -17,19 +17,19 @@ success() {
 
 explain "Configuration System"
 tell export LANG=de_DE.UTF-8
-tell echo $LANG UTF-8 >> /etc/locale.gen
-tell echo de_DE ISO-8859-1 >> /etc/locale.gen
-tell echo de_DE@euro ISO-8859-15 >> /etc/locale.gen
-tell echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+echo $LANG UTF-8 >> /etc/locale.gen
+echo de_DE ISO-8859-1 >> /etc/locale.gen
+echo de_DE@euro ISO-8859-15 >> /etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 tell locale-gen
-tell echo LANG=$LANG > /etc/locale.conf
-tell echo LC_TIME=$LANG >> /etc/locale.conf
-tell echo LC_COLLATE=C >> /etc/locale.conf
-tell echo LANGUAGE=de_DE >> /etc/locale.conf
-tell echo KEYMAP=de-latin1 > /etc/vconsole.conf
+echo LANG=$LANG > /etc/locale.conf
+echo LC_TIME=$LANG >> /etc/locale.conf
+echo LC_COLLATE=C >> /etc/locale.conf
+echo LANGUAGE=de_DE >> /etc/locale.conf
+echo KEYMAP=de-latin1 > /etc/vconsole.conf
 tell ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 tell hwclock --systohc
-tell echo dev-monster > /etc/hostname
+echo dev-monster > /etc/hostname
 tell systemctl enable dhcpcd.service
 tell passwd
 
@@ -45,7 +45,7 @@ tell sed -i 's/^FILES=.*/FILES=(\/crypto_keyfile.bin)/' /etc/mkinitcpio.conf
 tell mkinitcpio -p linux
 
 explain "Configure GRUB and Set the UUID of the encrypted root device"
-tell echo GRUB_ENABLE_CRYPTODISK=y >> /etc/default/grub
+echo GRUB_ENABLE_CRYPTODISK=y >> /etc/default/grub
 tell ROOTUUID=$(blkid /dev/sda1 | awk '{print $2}' | cut -d '"' -f2)
 tell sed -i "s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID="$ROOTUUID":lvm:allow-discards resume=\/dev\/mapper\/arch-swap\"/" /etc/default/grub
 tell grub-install /dev/sda
